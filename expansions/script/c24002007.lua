00d7e2f1e5ad76d509874a197995564663861821
Hello23-Ygopro <loleh32@hotmail.com>
2018-10-24 20:08:53 +0200

    move functions

--- expansions/script/c24002007.lua
+++ expansions/script/c24002007.lua
@@ -4,8 +4,6 @@ local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--draw
-	dm.AddSingleComeIntoPlayEffect(c,0,true,scard.drtg,scard.drop)
+	dm.AddSingleComeIntoPlayEffect(c,0,true,dm.DrawTarget(PLAYER_PLAYER),dm.DrawOperation(PLAYER_PLAYER,1))
 end
 scard.duel_masters_card=true
-scard.drtg=dm.DrawTarget(PLAYER_PLAYER)
-scard.drop=dm.DrawOperation(PLAYER_PLAYER,1)
--- expansions/script/c24004061.lua
+++ expansions/script/c24004061.lua
@@ -7,8 +7,6 @@ function scard.initial_effect(c)
 	--evolution
 	dm.AddEvolutionProcedure(c,aux.FilterBoolFunction(Card.IsDMRace,DM_RACE_CYBER_VIRUS))
 	--draw
-	dm.AddSingleComeIntoPlayEffect(c,0,true,scard.drtg,scard.drop)
+	dm.AddSingleComeIntoPlayEffect(c,0,true,dm.DrawTarget(PLAYER_PLAYER),dm.DrawOperation(PLAYER_PLAYER,3))
 end
 scard.duel_masters_card=true
-scard.drtg=dm.DrawTarget(PLAYER_PLAYER)
-scard.drop=dm.DrawOperation(PLAYER_PLAYER,3)
