b262b9ad4f5d082f73dbc9bf427535255f438049
Hello23-Ygopro <loleh32@hotmail.com>
2018-10-28 19:31:38 +0200

    comment

--- expansions/script/c24002001.lua
+++ expansions/script/c24002001.lua
@@ -3,7 +3,7 @@ local dm=require "expansions.utility_dmtcg"
 local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableSpellAttribute(c)
-	--ignore cannot attack
+	--get ability
 	dm.AddSpellCastEffect(c,0,nil,scard.abop)
 end
 scard.duel_masters_card=true
--- expansions/script/c24004057.lua
+++ expansions/script/c24004057.lua
@@ -9,11 +9,13 @@ function scard.initial_effect(c)
 	dm.EnableUpdatePower(c,2000,nil,DM_LOCATION_BATTLE,DM_LOCATION_BATTLE,aux.TargetBoolFunction(scard.abfilter))
 end
 scard.duel_masters_card=true
+--return
 function scard.retfilter(c)
 	return c:IsDMRace(DM_RACE_ANGEL_COMMAND) or c:IsDMRace(DM_RACE_DEMON_COMMAND) and c:IsAbleToHand()
 end
+scard.rettg=dm.CheckCardFunction(dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
+scard.retop=dm.SendtoHandOperation(nil,dm.DMGraveFilter(scard.abfilter),DM_LOCATION_GRAVE,0)
+--power up
 function scard.abfilter(c)
 	return c:IsDMRace(DM_RACE_ANGEL_COMMAND) or c:IsDMRace(DM_RACE_DEMON_COMMAND)
 end
-scard.rettg=dm.CheckCardFunction(dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
-scard.retop=dm.SendtoHandOperation(nil,dm.DMGraveFilter(scard.abfilter),DM_LOCATION_GRAVE,0)
