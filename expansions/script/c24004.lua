49926613fb661f8f84aec2f01abc54d14669bddc
Hello23-Ygopro <loleh32@hotmail.com>
2018-11-09 15:28:43 +0200

    adjust

--- expansions/script/c24004.lua
+++ expansions/script/c24004.lua
@@ -4,7 +4,7 @@ local dm=require "expansions.utility_dmtcg"
 local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
-	--tap ability (to grave)
+	--tap ability (search - to grave)
 	dm.EnableTapAbility(c,0,nil,dm.SendtoGraveOperation(PLAYER_PLAYER,Card.IsAbleToDMGrave,0,LOCATION_DECK,1))
 end
 scard.duel_masters_card=true
--- expansions/script/c24004058.lua
+++ expansions/script/c24004058.lua
@@ -8,10 +8,9 @@ function scard.initial_effect(c)
 	--double breaker
 	dm.EnableBreaker(c,DM_EFFECT_DOUBLE_BREAKER)
 	--destroy
-	dm.AddSingleComeIntoPlayEffect(c,0,nil,nil,scard.desop)
+	dm.AddSingleComeIntoPlayEffect(c,0,nil,nil,dm.DestroyOperation(nil,scard.desfilter,DM_LOCATION_BATTLE,DM_LOCATION_BATTLE))
 end
 scard.duel_masters_card=true
 function scard.desfilter(c)
 	return c:IsFaceup() and not c:IsCivilization(DM_CIVILIZATION_DARKNESS)
 end
-scard.desop=dm.DestroyOperation(nil,scard.desfilter,DM_LOCATION_BATTLE,DM_LOCATION_BATTLE)
