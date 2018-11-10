795061f0bb4e242cbd4aca13fd7ec2c6da8a7a73
Hello23-Ygopro <loleh32@hotmail.com>
2018-11-10 21:25:17 +0200

    add dm.SelfDestroyOperation

--- expansions/script/c24002032.lua
+++ expansions/script/c24002032.lua
@@ -10,12 +10,7 @@ function scard.initial_effect(c)
 	e1:SetDescription(aux.Stringid(sid,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_CUSTOM+DM_EVENT_ATTACK_PLAYER)
-	e1:SetOperation(scard.desop)
+	e1:SetOperation(dm.SelfDestroyOperation())
 	c:RegisterEffect(e1)
 end
 scard.duel_masters_card=true
-function scard.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.Destroy(c,REASON_EFFECT)
-end
