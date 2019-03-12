86540fb106a4391e436f4fcf7eabca2b548ed4c6
Hello23-Ygopro <loleh32@hotmail.com>
2019-03-12 14:55:32 +0200

    fix typo

--- expansions/script/c24004057.lua
+++ expansions/script/c24004057.lua
@@ -9,11 +9,13 @@ function scard.initial_effect(c)
 	dm.EnableUpdatePower(c,2000,nil,DM_LOCATION_BATTLE,DM_LOCATION_BATTLE,scard.powtg)
 end
 scard.duel_masters_card=true
-function scard.powtg(e,c)
-	return c:DMIsRace(DM_RACE_ANGEL_COMMAND) or c:DMIsRace(DM_RACE_DEMON_COMMAND)
-end
+--return
 function scard.retfilter(c)
-	return scard.powfilter(c) and c:IsAbleToHand()
+	return (c:DMIsRace(DM_RACE_ANGEL_COMMAND) or c:DMIsRace(DM_RACE_DEMON_COMMAND)) and c:IsAbleToHand()
 end
 scard.rettg=dm.CheckCardFunction(dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
 scard.retop=dm.SendtoHandOperation(nil,dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
+--power up
+function scard.powtg(e,c)
+	return c:DMIsRace(DM_RACE_ANGEL_COMMAND) or c:DMIsRace(DM_RACE_DEMON_COMMAND)
+end
--- expansions/script/c24011043.lua
+++ expansions/script/c24011043.lua
@@ -1,12 +1,12 @@
 --Hearty Cap'n Polligon
-local kj=require "expansions.utility_ktcg"
-local scard,sid=kj.GetID()
+local dm=require "expansions.utility_ktcg"
+local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--return
 	dm.EnableTurnEndSelfReturn(c,0,scard.retcon)
 end
-scard.kaijudo_card=true
+scard.duel_masters_card=true
 function scard.retcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetBrokenShieldCount()>0 and Duel.GetTurnPlayer()==tp
 end
