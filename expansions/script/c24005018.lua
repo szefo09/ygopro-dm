1bf08fdcfc4ad55ac67479100c04930540a331a1
Hello23-Ygopro <loleh32@hotmail.com>
2019-03-28 02:17:15 +0200

    update comments
    Updated "CIVILIZATION blocker" and "CIVILIZATION slayer" comments to make them consistent with "RACE hunter".

--- expansions/script/c24005018.lua
+++ expansions/script/c24005018.lua
@@ -3,7 +3,7 @@ local dm=require "expansions.utility_dmtcg"
 local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
-	--blocker
+	--blocker (fire and nature)
 	dm.EnableBlocker(c,nil,DM_DESC_FN_BLOCKER,aux.FilterBoolFunction(Card.IsCivilization,DM_CIVILIZATIONS_FN))
 end
 scard.duel_masters_card=true
--- expansions/script/c24005035.lua
+++ expansions/script/c24005035.lua
@@ -3,7 +3,7 @@ local dm=require "expansions.utility_dmtcg"
 local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
-	--slayer
+	--slayer (nature and light)
 	dm.EnableSlayer(c,nil,DM_DESC_NL_SLAYER,aux.FilterBoolFunction(Card.IsCivilization,DM_CIVILIZATIONS_LN))
 end
 scard.duel_masters_card=true
