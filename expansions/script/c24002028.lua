f946b7d169df20653eb1747293f099578f3e02d3
Hello23-Ygopro <loleh32@hotmail.com>
2019-03-29 09:10:05 +0200

    update Gigastand
    Added `dm.SingleDestroyReplaceTarget2` for its optional substitute effect.

--- expansions/script/c24002028.lua
+++ expansions/script/c24002028.lua
@@ -7,14 +7,9 @@ function scard.initial_effect(c)
 	dm.AddSingleDestroyReplaceEffect(c,0,scard.reptg,scard.repop)
 end
 scard.duel_masters_card=true
-function scard.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsReason(REASON_REPLACE) end
-	if e:GetHandler():IsAbleToHand() and Duel.SelectYesNo(tp,aux.Stringid(sid,1)) then
-		Duel.Hint(HINT_CARD,0,sid)
-		return true
-	else return false end
-end
+scard.reptg=dm.SingleDestroyReplaceTarget2(1,Card.IsAbleToHand)
 function scard.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,sid)
 	if Duel.SendtoHand(e:GetHandler(),PLAYER_OWNER,REASON_EFFECT+REASON_REPLACE)==0 then return end
 	Duel.ShuffleHand(tp)
 	Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT)
