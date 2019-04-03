519a741a9fff9144239d1c50c261763be4874969
Hello23-Ygopro <loleh32@hotmail.com>
2019-04-03 13:37:20 +0200

    fix Taunting Skyterror
    Added missing parameter.

--- expansions/script/c24010077.lua
+++ expansions/script/c24010077.lua
@@ -4,6 +4,6 @@ local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--attack if able
-	dm.EnableEffectCustom(c,EFFECT_MUST_ATTACK,aux.AND(dm.TurnPlayerCondition,dm.SelfTappedCondition),0,DM_LOCATION_BATTLE)
+	dm.EnableEffectCustom(c,EFFECT_MUST_ATTACK,aux.AND(dm.SelfTappedCondition,dm.TurnPlayerCondition(PLAYER_OPPO)),0,DM_LOCATION_BATTLE)
 end
 scard.duel_masters_card=true
