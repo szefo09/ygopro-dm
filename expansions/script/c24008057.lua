e12e024d5b719a0531aa357f0c3c8ea75d0adbf5
Hello23-Ygopro <loleh32@hotmail.com>
2019-04-09 22:50:53 +0200

    add dm.AddPlayerUseShieldTriggerEffect
    - Added `dm.AddPlayerUseShieldTriggerEffect` for cards with "Whenever a player uses the "shield trigger" ability of one of their shields".
    - Removed the unused condition parameter from `dm.AddPlayerCastSpellEffect`.
    - Fixed a bug where Ice Vapor, Shadow of Anguish didn't properly target cards.

--- expansions/script/c24008057.lua
+++ expansions/script/c24008057.lua
@@ -8,41 +8,6 @@ function scard.initial_effect(c)
 	--evolution
 	dm.AddEvolutionProcedure(c,aux.FilterBoolFunction(Card.DMIsEvolutionRace,DM_RACE_CYBER_LORD))
 	--draw
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(DM_LOCATION_BATTLE)
-	e1:SetOperation(scard.chop1)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(DM_LOCATION_BATTLE)
-	e2:SetOperation(scard.chop2)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(sid,0))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_CHAIN_END)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e3:SetRange(DM_LOCATION_BATTLE)
-	e3:SetCondition(scard.drcon)
-	e3:SetOperation(dm.DrawUpToOperation(PLAYER_SELF,2))
-	c:RegisterEffect(e3)
-	e1:SetLabelObject(e3)
-	e2:SetLabelObject(e3)
+	dm.AddPlayerUseShieldTriggerEffect(c,0,PLAYER_OPPO,nil,nil,dm.DrawUpToOperation(PLAYER_SELF,2))
 end
 scard.duel_masters_card=true
-function scard.chop1(e,tp,eg,ep,ev,re,r,rp)
-	e:GetLabelObject():SetLabel(0)
-end
-function scard.chop2(e,tp,eg,ep,ev,re,r,rp)
-	if rp==1-tp and re:IsHasCategory(DM_CATEGORY_SHIELD_TRIGGER) and re:GetHandler():IsBrokenShield() then
-		e:GetLabelObject():SetLabel(1)
-	end
-end
-function scard.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabel()==1
-end
--- expansions/script/c24009001.lua
+++ expansions/script/c24009001.lua
@@ -8,42 +8,8 @@ function scard.initial_effect(c)
 	--double breaker
 	dm.EnableBreaker(c,DM_EFFECT_DOUBLE_BREAKER)
 	--to shield
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(DM_LOCATION_BATTLE)
-	e1:SetOperation(scard.chop1)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(DM_LOCATION_BATTLE)
-	e2:SetOperation(scard.chop2)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(sid,0))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_CHAIN_END)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e3:SetRange(DM_LOCATION_BATTLE)
-	e3:SetCondition(scard.tscon)
-	e3:SetTarget(dm.CheckDeckFunction(PLAYER_SELF))
-	e3:SetOperation(dm.DecktopSendtoShieldOperation(PLAYER_SELF,1))
-	c:RegisterEffect(e3)
-	e1:SetLabelObject(e3)
-	e2:SetLabelObject(e3)
+	dm.AddPlayerUseShieldTriggerEffect(c,0,PLAYER_SELF,true,scard.tstg,scard.tsop)
 end
 scard.duel_masters_card=true
-function scard.chop1(e,tp,eg,ep,ev,re,r,rp)
-	e:GetLabelObject():SetLabel(0)
-end
-function scard.chop2(e,tp,eg,ep,ev,re,r,rp)
-	if rp==1-tp and re:IsHasCategory(DM_CATEGORY_SHIELD_TRIGGER) and re:GetHandler():IsBrokenShield() then
-		e:GetLabelObject():SetLabel(1)
-	end
-end
-function scard.tscon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabel()==1
-end
+scard.tstg=dm.CheckDeckFunction(PLAYER_SELF)
+scard.tsop=dm.DecktopSendtoShieldOperation(PLAYER_SELF,1)
