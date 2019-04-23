6e4d640cf03320c25b1e1800800cd2bf6ba62a49
Hello23-Ygopro <loleh32@hotmail.com>
2019-04-23 15:41:13 +0200

    fix Headlong Giant
    Q: This creature attacks while I have only one card in my hand. I discard that card, so now I have no cards in my hand. Does Headlong Giant stop attacking?
    A: No, it continues the attack. If you have no cards in your hand, Headlong Giant can't start an attack. But once it starts attacking, the number of cards in your hand doesn't matter.
    https://duelmasters.fandom.com/wiki/Headlong_Giant/Rulings

--- expansions/script/c24007060.lua
+++ expansions/script/c24007060.lua
@@ -6,11 +6,28 @@ function scard.initial_effect(c)
 	--cannot attack
 	dm.EnableCannotAttack(c,dm.NoHandCondition(PLAYER_SELF))
 	dm.AddEffectDescription(c,1,dm.NoHandCondition(PLAYER_SELF))
-	--discard
-	dm.AddSingleAttackTriggerEffect(c,0,nil,nil,dm.DiscardOperation(PLAYER_SELF,aux.TRUE,LOCATION_HAND,0,1))
+	--attack cost (discard)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ATTACK_COST)
+	e1:SetCost(scard.atcost)
+	e1:SetOperation(scard.atop)
+	c:RegisterEffect(e1)
 	--cannot be blocked
 	dm.EnableCannotBeBlocked(c,dm.CannotBeBlockedBoolFunction(Card.IsPowerBelow,4000))
 	--triple breaker
 	dm.EnableBreaker(c,DM_EFFECT_TRIPLE_BREAKER)
 end
 scard.duel_masters_card=true
+function scard.atcost(e,c,tp)
+	return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+end
+function scard.atop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST)
+	Duel.AttackCostPaid()
+end
+--[[
+	References
+		1. The Dragon's Bead
+		https://github.com/Fluorohydride/ygopro-scripts/blob/f24eb49/c92408984.lua#L37
+]]
