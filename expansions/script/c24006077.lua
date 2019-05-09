4426650b829a5aff785a7ca4f677ddb3cc6c6af8
Hello23-Ygopro <loleh32@hotmail.com>
2019-05-09 19:23:24 +0200

    fix Cocco Lupia
    Fixed a typo that caused an error.

--- expansions/script/c24006077.lua
+++ expansions/script/c24006077.lua
@@ -4,10 +4,10 @@ local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--mana cost down
-	dm.EnableUpdateManaCost(c,scard.mcval,LOCATION_ALL,0,aux.TargetBoolFunction(Card.DMIsRace(DM_RACE_DRAGON)))
+	dm.EnableUpdateManaCost(c,scard.mcval,LOCATION_ALL,0,aux.TargetBoolFunction(Card.DMIsRace,DM_RACE_DRAGON))
 end
 scard.duel_masters_card=true
 function scard.mcval(e,c)
-	if c:GetManaCost()<=2 then return 0
+	if c:GetOriginalManaCost()<=2 then return 0
 	else return -2 end
 end
