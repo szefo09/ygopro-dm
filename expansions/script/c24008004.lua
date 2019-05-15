10f7b0222b9f287c374ef96a71e32cea50ab346b
Hello23-Ygopro <loleh32@hotmail.com>
2019-05-15 20:43:54 +0200

    add reset
    Added a reset condition to the continuous effect instead of only using a condition function.
    https://github.com/Hello23-Ygopro/ygopro-dm/commit/c1fc678c82c71efb26f43d73e973c60956ef390d

--- expansions/script/c24008004.lua
+++ expansions/script/c24008004.lua
@@ -18,13 +18,33 @@ function scard.abop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
-	e2:SetCondition(scard.sacon)
 	e2:SetTargetRange(DM_LOCATION_BATTLE,0)
 	e2:SetLabelObject(e1)
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e2,tp)
+	--reset speed attacker
+	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_ADJUST)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCountLimit(1)
+	e3:SetLabelObject(e2)
+	e3:SetCondition(scard.rstcon)
+	e3:SetOperation(scard.rstop)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
 end
-function scard.sacon(e)
+function scard.rstcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsFaceup() and c:IsLocation(DM_LOCATION_BATTLE)
+	return c:IsFacedown() or not c:IsLocation(DM_LOCATION_BATTLE)
 end
+function scard.rstop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=e:GetLabelObject()
+	e1:Reset()
+	e:Reset()
+end
+--[[
+	Notes
+		1. The speed attacker effect lasts as long as Magmadragon Jagalzor is in the battle zone
+		https://duelmasters.fandom.com/wiki/Magmadragon_Jagalzor/Rulings
+]]
