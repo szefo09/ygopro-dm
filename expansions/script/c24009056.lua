c1fc678c82c71efb26f43d73e973c60956ef390d
Hello23-Ygopro <loleh32@hotmail.com>
2019-05-15 20:29:34 +0200

    fix Petrova, Channeler of Suns
    - Petrova, Channeler of Suns should also give creatures +4000 power that are put into the battle zone after its come into play ability has resolved.
    - The power increase lasts as long as Petrova is in the battle zone.
    https://duelmasters.fandom.com/wiki/Petrova,_Channeler_of_Suns/Rulings

--- expansions/script/c24009056.lua
+++ expansions/script/c24009056.lua
@@ -32,10 +32,42 @@ function scard.abop(e,tp,eg,ep,ev,re,r,rp)
 	local race=Duel.DMAnnounceRace(tp)
 	table.insert(t1,1,DM_SELECT_RACE_MECHA_DEL_SOL)
 	table.insert(t2,1,DM_RACE_MECHA_DEL_SOL)
-	local g=Duel.GetMatchingGroup(scard.abfilter,tp,DM_LOCATION_BATTLE,DM_LOCATION_BATTLE,nil,race)
-	if g:GetCount()==0 then return end
-	for tc in aux.Next(g) do
-		--power up
-		dm.RegisterEffectUpdatePower(e:GetHandler(),tc,1,4000,0,0)
-	end
+	--update power
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(sid,1))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(DM_EFFECT_UPDATE_POWER)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetValue(4000)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e2:SetTargetRange(DM_LOCATION_BATTLE,DM_LOCATION_BATTLE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.DMIsRace,race))
+	e2:SetLabelObject(e1)
+	Duel.RegisterEffect(e2,tp)
+	--reset update power
+	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_ADJUST)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCountLimit(1)
+	e3:SetLabelObject(e2)
+	e3:SetCondition(scard.rstcon)
+	e3:SetOperation(scard.rstop)
+	Duel.RegisterEffect(e3,tp)
 end
+function scard.rstcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsFacedown() or not c:IsLocation(DM_LOCATION_BATTLE)
+end
+function scard.rstop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=e:GetLabelObject()
+	e1:Reset()
+	e:Reset()
+end
+--[[
+	Notes
+		1. The power increase effect lasts as long as Petrova, Channeler of Suns is in the battle zone
+		https://duelmasters.fandom.com/wiki/Petrova,_Channeler_of_Suns/Rulings
+
+]]
