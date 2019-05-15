181c4add31b93c9a4ff28f83de31c7006298c665
Hello23-Ygopro <loleh32@hotmail.com>
2019-05-15 20:02:13 +0200

    fix Turtle Horn, the Imposing error

--- expansions/script/c24012053.lua
+++ expansions/script/c24012053.lua
@@ -4,6 +4,6 @@ local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--search (to hand)
-	dm.AddPlayerUseShieldTriggerEffect(c,0,PLAYER_OPPO,nil,nil,dm.SendtoHandOperation(tp,Card.IsCreature,LOCATION_DECK,0,0,1,true))
+	dm.AddPlayerUseShieldTriggerEffect(c,0,PLAYER_OPPO,nil,nil,dm.SendtoHandOperation(PLAYER_SELF,Card.IsCreature,LOCATION_DECK,0,0,1,true))
 end
 scard.duel_masters_card=true
