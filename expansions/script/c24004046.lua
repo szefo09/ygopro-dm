8f68af063caba5b61f3d92e0e0b7361833e53623
Hello23-Ygopro <loleh32@hotmail.com>
2019-05-28 18:23:47 +0200

    add aux.TargetBoolFunction
    https://github.com/Hello23-Ygopro/ygopro-dm/commit/9eeb9f1b37cfb5ad4d647954732d2509c12ab56d

--- expansions/script/c24004046.lua
+++ expansions/script/c24004046.lua
@@ -4,9 +4,6 @@ local scard,sid=dm.GetID()
 function scard.initial_effect(c)
 	dm.EnableCreatureAttribute(c)
 	--power up
-	dm.EnableUpdatePower(c,1000,nil,DM_LOCATION_BZONE,DM_LOCATION_BZONE,scard.powtg)
+	dm.EnableUpdatePower(c,1000,nil,DM_LOCATION_BZONE,DM_LOCATION_BZONE,aux.TargetBoolFunction(Card.DMIsRace,DM_RACE_ARMORED_DRAGON))
 end
 scard.duel_masters_card=true
-function scard.powtg(e,c)
-	return c:IsFaceup() and c:DMIsRace(DM_RACE_ARMORED_DRAGON)
-end
--- expansions/script/c24004057.lua
+++ expansions/script/c24004057.lua
@@ -6,16 +6,11 @@ function scard.initial_effect(c)
 	--return
 	dm.AddSingleComeIntoPlayTriggerEffect(c,0,true,scard.rettg,scard.retop)
 	--power up
-	dm.EnableUpdatePower(c,2000,nil,DM_LOCATION_BZONE,DM_LOCATION_BZONE,scard.powtg)
+	dm.EnableUpdatePower(c,2000,nil,DM_LOCATION_BZONE,DM_LOCATION_BZONE,aux.TargetBoolFunction(Card.DMIsRace,DM_RACE_ANGEL_COMMAND,DM_RACE_DEMON_COMMAND))
 end
 scard.duel_masters_card=true
---return
 function scard.retfilter(c)
 	return c:DMIsRace(DM_RACE_ANGEL_COMMAND,DM_RACE_DEMON_COMMAND) and c:IsAbleToHand()
 end
 scard.rettg=dm.CheckCardFunction(dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
 scard.retop=dm.SendtoHandOperation(nil,dm.DMGraveFilter(scard.retfilter),DM_LOCATION_GRAVE,0)
---power up
-function scard.powtg(e,c)
-	return c:DMIsRace(DM_RACE_ANGEL_COMMAND,DM_RACE_DEMON_COMMAND)
-end
